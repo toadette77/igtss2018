9e159198f7b7b8dd31078c369003565d01c47c37
toadette77 <toadette007@outlook.de>
2018-04-11 13:53:28 +0200

    maven project with hibernate and jersey

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer")
+public class Customer implements Serializable {
+    // member variables
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private int id;
+    
+    @Column(name="name")
+    private String name;
+    
+    @Column(name="password")
+    private String password;
+    
+    // getters & setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
